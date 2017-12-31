7fbf5b2928134912665542a09a0f2e57b2e81878
GarrysGaming <34984861+GarrysGaming@users.noreply.github.com>
2017-12-31 20:01:44 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,6 +2,8 @@
 
     const Discord = require("discord.js");
 
+
+
     var bot = new Discord.Client()
 
     bot.on("ready", function(){
@@ -14,24 +16,26 @@
     bot.on('message', function (message){
         if(message.channel.name === 'bot'){
             if(message.content === '!help')   {
+                message.delete(message.author)
                 message.channel.sendMessage('Voici la liste des commandes disponible : !help ; !yt')
             }
-            if(message.content === '!yt'){
+            else if(message.content === '!yt'){
+                message.delete(message.author)
                 message.channel.sendMessage('Voici la chaine youtube de GarrysGaming : https://www.youtube.com/channel/UCu80F8H_BDuVjIrXnvWUjew')
             }
         }
         if(message.channel.name === 'music'){
-            if(message.content === '.music'){
-                message.delete(message.author)
-               message.chanel.sendMessage('Fait .music play <lien/nom>')
-               }
             if(message.content === '!play'){
+                message.delete(message.author)
                 message.channel.sendMessage('Fonction bientôt disponible !')
             }
         }
     })
     bot.on('guildMemberAdd', function (membre){
-        membre.sendMessage('Bienvenue ' + membre.displayName + ' sur le discord de GarrysGaming !')
+        membre.sendMessage('Bienvenue ' + membre.displayName + ' sur le discord ' + membre.guild.name)
     })
+
+
+
 
 
