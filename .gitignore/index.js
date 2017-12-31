020e6d28edc7cf7f50536a9a99fdf08dcb8ef366
GarrysGaming <34984861+GarrysGaming@users.noreply.github.com>
2017-12-31 16:08:15 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+    const Discord = require("discord.js");
+
+    var bot = new Discord.Client();
+
+    bot.on("ready", function(){
+        bot.user.setGame("GarrysBot, !help");
+    });
+
+    bot.login("Mzk2MzA5OTU0NTUxODczNTM3.DSqMUw.AmWxggPBMEbaWsToNfalrdd3gHQ");
